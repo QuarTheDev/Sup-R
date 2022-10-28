760d78fd6d80861158e79d942e18ce4e2a06f6e1
Dabia <70469187+DabiaTheNord@users.noreply.github.com>
2022-10-27 23:22:38 -0400

    Create remv.sh

---
+++ uninstall/remv.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+clear
+echo "Please wait..."
+
+DEP1="sed"
+DEP2="xdg-open"
+DEP3="zenity"
+GDISTRO=$(sed -n '2p' /etc/os-release | sed 's/NAME=//' | sed 's/\"//g')
+
+clear
+
+echo " __        __      __                       __  ___                 ___  __  "
+echo "/__\` |  | |__) __ |__)    |  | |\ | | |\ | /__\`  |   /\  |    |    |__  |__) "
+echo ".__/ \__/ |       |  \    \__/ | \| | | \| .__/  |  /~~\ |___ |___ |___ |  \ "
+echo "for $GDISTRO"
+
+sleep 1s
+
+#Dependencies check
+
+echo -e "\n\nChecking dependencies...\n"
+command -v $DEP1 >/dev/null || { echo -e "\n\n$DEP1 was not found, installation canceled.\n\nTry running 'sudo apt install $DEP1'." >&2; exit 1; }
+command -v $DEP2 >/dev/null || { echo -e "\n\n$DEP2 was not found, installation canceled.\n\nTry running 'sudo apt install $DEP2'." >&2; exit 1; }
+command -v $DEP3 >/dev/null || { echo -e "\n\n$DEP3 was not found, installation canceled.\n\nTry running 'sudo apt install $DEP3'." >&2; exit 1; }
+
+echo -e "OK\n"
+
+sleep 0.5s
+
+echo -e "Sup-'R' will be removed."
+read -rsn1 -p "When ready, press any key to uninstall."; echo
+
+TIMEFORMAT='Deleted 5.59kb of files in %2R seconds.'
+
+time {
+echo -e "\nSetting up removal directory...\n"
+rm -f -r ~/.run-prompt/
+}
+
+echo -e "Done!\n"
+echo -e "SUP-R has been uninstalled.\n"
+sleep 1s
+echo -e "Exiting..."
+exit
